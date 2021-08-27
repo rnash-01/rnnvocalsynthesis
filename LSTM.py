1c4edc4beabdc174ba4a12c06aeec4131dcd1f32
Raphael <raphaelcst8@gmail.com>
2021-08-27 18:24:56 +0100

    Began work on gradient calculation

--- LSTM.py
+++ LSTM.py
@@ -14,18 +14,19 @@ class LSTM():
 
         # * input and 'previous' output concatenated together.
 
+        self.parameters = {}
         # Weights
 
-        self.weights_forget = np.random.randn(output_size, input_size + output_size)
-        self.weights_in_gate = np.random.randn(output_size, input_size + output_size)
-        self.weights_remember = np.random.randn(output_size, input_size + output_size)
-        self.weights_select = np.random.randn(output_size, input_size + output_size)
+        self.parameters["weights_forget"] = np.random.randn(output_size, input_size + output_size)
+        self.parameters["weights_in_gate"] = np.random.randn(output_size, input_size + output_size)
+        self.parameters["weights_remember"] = np.random.randn(output_size, input_size + output_size)
+        self.parameters["weights_select"] = np.random.randn(output_size, input_size + output_size)
 
         # Biases
-        self.biases_forget = np.zeros((output_size, 1))
-        self.biases_in_gate = np.zeros((output_size, 1))
-        self.biases_remember = np.zeros((output_size, 1))
-        self.biases_select = np.zeros((output_size, 1))
+        self.parameters["biases_forget"] = np.zeros((output_size, 1))
+        self.parameters["biases_in_gate"] = np.zeros((output_size, 1))
+        self.parameters["biases_remember"] = np.zeros((output_size, 1))
+        self.parameters["biases_select"] = np.zeros((output_size, 1))
 
 
         # Additional state information
@@ -73,22 +74,22 @@ class LSTM():
             self.input_and_output = np.concatenate((self.output, self.input), axis=0)
 
             # Forget gate
-            self.pre_forget = np.dot(self.weights_forget, self.input_and_output)
+            self.pre_forget = np.dot(self.parameters["weights_forget"], self.input_and_output) + self.parameters["biases_forget"]
             self.forget = self.sigmoid(self.pre_forget)
 
             self.state_multiplied = np.multiply(self.forget, self.state)
 
             # Remember
-            self.pre_in_gate = np.dot(self.weights_in_gate, self.input_and_output)
+            self.pre_in_gate = np.dot(self.parameters["weights_in_gate"], self.input_and_output) + self.parameters["biases_in_gate"]
             self.in_gate = self.sigmoid(self.pre_in_gate)
-            self.pre_remember = np.dot(self.weights_remember, self.input_and_output)
+            self.pre_remember = np.dot(self.parameters["weights_remember"], self.input_and_output) + self.parameters["biases_remember"]
             self.remember = np.tanh(self.pre_remember)
 
             self.state_added = self.state_multiplied + self.in_gate
 
             # Select
             self.state_tanh = np.tanh(self.state_added)
-            self.pre_select = np.dot(self.weights_select, self.input_and_output)
+            self.pre_select = np.dot(self.parameters["weights_select"], self.input_and_output) + self.parameters["biases_select"]
             self.select = self.sigmoid(self.pre_select)
 
             self.output = self.select * self.state_tanh
@@ -123,6 +124,89 @@ class LSTM():
         cache["predictions"] = predictions
 
         return cache
+
+    def calculate_gradients(self, Y, t, cache):
+        # Get all cache elements
+        input_and_output = cache["input_and_output"]
+        pre_forget = cache["pre_forget"]
+        forget = cache["forget"]
+        pre_in_gate = cache["pre_in_gate"]
+        in_gate = cache["in_gate"]
+        pre_remember = cache["pre_remember"]
+        remember = cache["remember"]
+        pre_select = cache["pre_select"]
+        select = cache["select"]
+        state_multiplied = cache["state_multiplied"]
+        state_added = cache["state_added"]
+        state_tanh = cache["state_tanh"]
+        predictions = cache["predictions"]
+
+
+        # Get parameters
+        weights_forget = self.parameters["weights_forget"]
+        weights_in_gate = self.parameters["weights_in_gate"]
+        weights_remember = self.parameters["weights_remember"]
+        weights_select = self.parameters["weights_select"]
+
+        biases_forget = self.parameters["biases_forget"]
+        biases_in_gate = self.parameters["biases_in_gate"]
+        biases_remember = self.parameters["biases_remmeber"]
+        biases_select = self.parameters["biases_select"]
+
+        # Set up gradient matrices
+        dWf = np.zeros(weights_forget.shape)
+        dWi = np.zeros(weights_in_gate.shape)
+        dWr = np.zeros(weights_remember.shape)
+        dWs = np.zeros(weights_remember.shape)
+
+        dbf = np.zeros(biases_forget.shape)
+        dbi = np.zeros(biases_in_gate.shape)
+        dbr = np.zeros(biases_remember.shape)
+        dbs = np.zeros(biases_select.shape)
+
+        for i in range(t):
+            time_step = t - 1 - i
+
+            # Get important states
+            prediction_t = predictions[time_step]
+            pre_select_t = pre_select[time_step]
+            pre_remember_t = pre_remember[time_step]
+            pre_in_gate_t = pre_in_gate[time_step]
+            pre_forget_t = pre_forget[time_step]
+            input_and_output_t = input_and_output[time_step]
+            state_current = state_added[time_step]
+
+            if(time_step == 0):
+                state_previous = np.zeros((self.output_size, Y.shape[1]//t))
+            else:
+                state_previous = state_added[time_step - 1]
+
+            # Calculate derivatives
+            # First, sort out dh_t
+            true_t = Y[time_step]
+            dh_t = -2/(t * prediction.shape[0]) * (true_t - prediction_t)
+
+            # Initialise dc_t if not already done
+            if(time_step == t - 1):
+                dc_t = dh_t * self.sigmoid(pre_select_t)
+
+            # Calculate remember gate gradient
+            dr = dc_t
+            dp_r = dr * self.sigmoid(pre_select_t) * self.tanh_prime(pre_remember_t)
+            dWr = dWr + np.dot(np_r, input_and_output_t.T)
+
+            # Calculate input gate gradient
+            dp_i = dr * np.tanh(pre_remember_t) * self.sigmoid_prime(pre_in_gate_t)
+            dWi = dWi + np.dot(dp_i, input_and_output_t.T)
+
+            # Calculate forget gate gradient
+            df = dc_t * state_previous
+            dp_f = df * self.sigmoid_prime(pre_forget_t)
+            dWf = dWf + np.dot(dp_f, input_and_output_t.T)
+
+            dp_s = dh_t * np.tanh(state_current) * self.sigmoid_prime(pre_select_t)
+            dWs = dWs + np.dot(dp_s, input_and_output_t.T)
+
 
 
     def segment_data(self, t, data):
